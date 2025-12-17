91ab663b8c407e0d7095c1578edc94a0a4f4eb55
Pritam Basak <akariju@Pritams-MacBook-Air.local>
2025-12-17 15:46:06 +0530

    17/12/25

---
+++ problems/900/A_Make_It_Zero.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);
+#define int long long
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+int32_t main()
+{
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int &num : a)
+            cin >> num;
+        if (!(n & 1))
+        {
+            cout << 2 << endl;
+            cout << 1 << " " << n << endl;
+            cout << 1 << " " << n << endl;
+        }else{
+            cout << 4 << endl;
+            cout << 1 << " " << n-1 << endl;
+            cout << 1 << " " << n-1 << endl;
+            cout << n-1 << " " << n << endl;
+            cout << n-1 << " " << n << endl;
+        }
+    }
+    return 0;
+}
